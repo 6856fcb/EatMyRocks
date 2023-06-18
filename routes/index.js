02d1d19044ecb75996f4b7ff358c96f210a9142c
alexvujisic <alexandar.vujisic@stud.fh-campuswien.ac.at>
2023-06-18 18:10:24 +0200

    Created JSON and implement in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const product = require('./product.js');
 
 // BSP verwendung vom Router
 router.get('/', (req, res) => {
@@ -11,29 +11,32 @@ router.get('/', (req, res) => {
         references: ['About Us', 'Contact', 'FAQ'],
         products: [
           { 
-            title: 'Product 1',
-            description: 'This is the description of product 1.',
-            image: 'product1.jpg'
+            title: product.products[0].name,
+            description: product.products[0].description,
+            image: 'product1.jpg',
+            price: product.products[0].price
           },
           {
-            title: 'Product 2',
-            description: 'This is the description of product 2.',
-            image: 'product2.jpg'
+            title: product.products[1].name,
+            description: product.products[1].description,
+            image: 'product2.jpg',
+            price: product.products[1].price
           }
         ],
         recipes: [
           {
-            title: 'Recipe 1',
-            description: 'This is the description of recipe 1.',
+            title: 'Recipe for ', //+ product.products[0].name,
+            description: product.products[0].recipe,
             image: 'recipe1.jpg'
           },
           {
-            title: 'Recipe 2',
-            description: 'This is the description of recipe 2.',
+            title: 'Recipe for ',// + product.products[1].name,
+            description: product.products[1].recipe,
             image: 'recipe2.jpg'
           }
         ]
       });
+      //console.log(product.products[1].name);
     } catch (error) {
       console.error(error);
       res.status(500).send('Internal Server Error');
